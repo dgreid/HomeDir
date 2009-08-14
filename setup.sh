b0dd40a753576b481b545142c34f2f82c445411f
Dylan Reid <dylan@Dlaptop.eternia>
2009-08-14 01:33:46 -0400

    don't symlink the .git repository to the homedir in the setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PWD=`pwd`
-for i in `find . -maxdepth 1 -name '*[a-zA-Z]*' | grep -v setup.sh`
+for i in `find . -maxdepth 1 -name '*[a-zA-Z]*' | grep -v setup.sh | grep -v '.git'`
 do
 	ln -s $PWD/$i ~/$i
 done
