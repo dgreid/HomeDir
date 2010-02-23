5dfbcc1e9eefad8203c37805ac29b32c4757f28a
Dylan Reid <dgreid@gmail.com>
2010-02-22 20:06:22 -0500

    use zenburn theme for awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -9,7 +9,7 @@ require("naughty")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init(os.getenv("HOME") .. "/.config/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm -sb -sl 9999 -fg green -bg black"
