0bffbf35ab1292a38ffe5c7d9903e1701c9b5e2b
Dylan Reid <dgreid@gmail.com>
2010-02-22 19:54:07 -0500

    awesomerc: firefox just opens on tag 6

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -297,9 +297,9 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
+    -- Set Firefox to always map on tags number 6 of screen 1.
+    { rule = { class = "Namoroka" },
+      properties = { tag = tags[1][6] } },
 }
 -- }}}
 
