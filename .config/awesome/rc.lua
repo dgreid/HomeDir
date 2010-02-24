1d34710562a0034d69d73c68a4d360d83b0bd2a5
Dylan Reid <dylan@devel32.localdomain>
2010-02-24 01:12:01 -0500

    awesome: W-S-delete locks screen

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -195,6 +195,7 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
+    awful.key({ modkey, "Shift"   }, "Delete", function () awful.util.spawn("xscreensaver-command -l") end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
