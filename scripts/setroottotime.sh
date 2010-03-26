7fd011925dee3dc7319d1d0f74860f499036d410
Dylan Reid <dgreid@gmail.com>
2010-03-26 17:22:31 -0400

    dwm: add battery status to the status bar.

--- scripts/setroottotime.sh
+++ scripts/setroottotime.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true
 do
-   xsetroot -name "`date "+%b %d %H:%M"`"
+   xsetroot -name "$( acpi -b | awk '{ print $3 $4 }' | tr -d ',' ) $( date "+%b %d %H:%M" )"
    sleep 10
 done
