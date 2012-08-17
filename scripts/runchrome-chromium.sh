a7d9e61e588af094a49ff90273953bf26dd120a8
Dylan Reid <dgreid@google.com>
2012-08-16 20:54:03 -0700

    update runchrome scripts

--- scripts/runchrome-chromium.sh
+++ scripts/runchrome-chromium.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-google-chrome --user-data-dir=/home/dgreid/.config/google-chrome-chromium --proxy-pac-url=http://proxyconfig.corp.google.com/wpad.dat 2>&1 > /dev/null
+google-chrome --use-cras --enable-platform-apps --enable-media-stream --user-data-dir=/home/dgreid/.config/google-chrome-chromium --proxy-pac-url=http://proxyconfig.corp.google.com/wpad.dat 2>&1 > /dev/null
--- scripts/runchrome.sh
+++ scripts/runchrome.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
- google-chrome --proxy-pac-url=http://proxyconfig.corp.google.com/wpad.dat 2>&1 > /dev/null
+google-chrome --use-cras --proxy-pac-url=http://proxyconfig.corp.google.com/wpad.dat 2>&1 > /dev/null
