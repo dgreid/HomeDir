05eb33028abbaaeb7e8243eae2bf8939d41d5dc3
Dylan Reid <dgreid@chromium.org>
2018-12-13 17:36:34 +0000

    add ssh config for github

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 PWD=`pwd`
-for i in `find . -maxdepth 1 -name '*[a-zA-Z]*' | grep -v setup.sh | grep -v '.git'`
+for i in `find . -maxdepth 1 -name '*[a-zA-Z]*' | grep -v setup.sh | grep -v '.git' | grep -v '.ssh'`
 do
 	rm -f ~/$i
 	ln -s $PWD/$i ~/$i
 done
 ln -s $PWD/.git-completion.bash ~
 ln -s $PWD/.gitconfig ~
+mkdir -p ~/.ssh
+ln -s $PWD/.ssh/config ~/.ssh/config
