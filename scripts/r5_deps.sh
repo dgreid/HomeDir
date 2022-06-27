4e38bef21778d01124a66ce628d24daf55c53d86
Dylan Reid <dgreid@rivosinc.com>
2022-06-27 15:02:53 -0700

    Add r5_deps script
    To help install dev dependencies.

---
+++ scripts/r5_deps.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# r5
+sudo apt install gcc-riscv64-unknown-elf g++-riscv64-linux-gnu
+
+# qemu
+sudo apt-get install git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build
