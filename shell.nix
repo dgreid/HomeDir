4a389c1f3f7a9799d6763c43f009512d6c6047e5
Dylan Reid <dgreid@rivosinc.com>
2023-10-21 13:46:19 +0000

    Add basic shell.nix file
    Can build the kernel and crosvm to start.

---
+++ shell.nix
@@ -0,0 +1,42 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [
+        autoconf
+        automake
+        bc
+        binutils
+        bison
+        bzip2
+        clang
+        clang-tools
+	crosvm
+	crosvm.buildInputs
+	crosvm.nativeBuildInputs
+        curl
+        flex
+        gdb
+        gitFull
+        gnumake
+        gnupg
+        lynx
+        ncurses
+        neomutt
+        neovim
+        nodejs
+        openssh
+        openvpn
+        pass
+        podman
+        tmux
+        wget
+
+# kernel builds
+	#kernel.buildInputs
+ perl gmp libmpc mpfr pkg-config ncurses perl bc nettools openssl rsync zstd python3 kmod
+    ];
+     # why do we need to set the library path manually?
+    shellHook = ''
+      export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
+    '';
+}
