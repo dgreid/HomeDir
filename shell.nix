c2b503f77998d67b741d759d53f74ad32b329a22
Dylan Reid <dgreid@rivosinc.com>
2024-12-22 20:36:08 -0800

    strip non-mac packages from shell

--- shell.nix
+++ shell.nix
@@ -6,48 +6,24 @@ in
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = with pkgs.buildPackages; [
-        autoconf
-        automake
         bc
-        binutils
-        bison
-        bzip2
-        clang
-        clang-tools
-	crosvm
 #	crosvm.buildInputs
 #	crosvm.nativeBuildInputs
-        curl
 	delta
-        flex
         fzf
-        gdb
         gitFull
         gnumake
         gnupg
-        lynx
 	meson
         ncurses
         neomutt
         latestPkgs.neovim
         nodejs
         openssh
-        openvpn
         pass
-        perf-tools
-	pkg-config
-        pkgs.linuxPackages_latest.perf
-        podman
 	rustup
         tmux
         wget
 
-# kernel builds
-	#kernel.buildInputs
- perl gmp libmpc mpfr pkg-config ncurses perl bc nettools openssl rsync zstd python3 kmod
     ];
-     # why do we need to set the library path manually?
-    shellHook = ''
-      export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
-    '';
 }
