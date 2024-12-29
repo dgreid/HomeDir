5df104edc9cb96264e01450af8c1680b57eb5043
Dylan Reid <dgreid@dylanreid.com>
2024-12-28 20:32:09 -0800

    tmux split automatic

--- scripts/tmux_split.sh
+++ scripts/tmux_split.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Get the current width of the terminal.
-width=$(tput cols)
+width=$(tmux display-message -p '#{pane_width}')
 
 # If the widths is less than 3, exit.
 if [ $width -lt 3 ]; then
@@ -9,9 +9,16 @@ if [ $width -lt 3 ]; then
     exit 1
 fi
 
+# The default center width is 106 if width < 220, otherwise 212.
+if [ $width -lt 220 ]; then
+    default_width=106
+else
+    default_width=212
+fi
+
 # First argument to the script is the center pane width.
 # If no argument is given, the default is 106.
-center_width=${1:-106}
+center_width=${1:-$default_width}
 
 # left and right must each be at least 1. If center is too wide, set it to the total width -2.
 if [ $center_width -gt $((width - 2)) ]; then
