3c3d9330157197813c47d8bde326b6a779a5a086
Dylan Reid <dgreid@dylanreid.com>
2024-12-29 00:38:38 -0800

    tmux: resize splits on window resize

--- scripts/tmux_split.sh
+++ scripts/tmux_split.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Get the current width of the terminal.
-width=$(tmux display-message -p '#{pane_width}')
+width=$(tmux display-message -p '#{window_width}')
 
 # If the widths is less than 3, exit.
 if [ $width -lt 3 ]; then
@@ -15,25 +15,8 @@ if [ $(tmux list-panes | wc -l) -gt 1 ]; then
     exit 1
 fi
 
-# The default center width is 106 if width < 220, otherwise 212.
-if [ $width -lt 220 ]; then
-    default_width=106
-else
-    default_width=212
-fi
-
-# First argument to the script is the center pane width.
-# If no argument is given, the default is 106.
-center_width=${1:-$default_width}
-
-# left and right must each be at least 1. If center is too wide, set it to the total width -2.
-if [ $center_width -gt $((width - 2)) ]; then
-    center_width=$((width - 2))
-fi
-
-side_width=$(((width - center_width) / 2))
-
 # Split the current pane into three panes.
-tmux split-window -h -l $side_width
-tmux split-window -h -l $center_width -t 0
+tmux split-window -h
+tmux split-window -h
+~/scripts/tmux_split_resize.sh
 tmux select-pane -t 1
---
+++ scripts/tmux_split_resize.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Get the current width of the terminal.
+width=$(tmux display-message -p '#{window_width}')
+
+echo $width
+
+# If the widths is less than 3, exit.
+if [ $width -lt 3 ]; then
+    exit 0
+fi
+
+# if there aren't three panes, exit.
+if [ $(tmux list-panes | wc -l) -ne 3 ]; then
+    exit 0
+fi
+
+# The default center width is 106 if width < 220, otherwise 212.
+if [ $width -lt 220 ]; then
+    center_width=106
+else
+    center_width=212
+fi
+
+# left and right must each be at least 1. If center is too wide, set it to the total width -2.
+if [ $center_width -gt $((width - 2)) ]; then
+    center_width=$((width - 2))
+fi
+
+side_width=$(((width - center_width) / 2))
+
+tmux resize-pane -x $side_width -t 2
+tmux resize-pane -x $side_width -t 0
