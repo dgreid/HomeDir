de6fd84e41e7f9225e554d3fb9d81318f62de867
Dylan Reid <dgreid@dylanreid.com>
2025-01-01 22:42:55 -0800

    aerospace init

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 PWD=$(pwd)
 for i in $(find . -maxdepth 1 -name '*[a-zA-Z]*' | grep -v setup.sh | grep -v '.git' | grep -v '.config' | grep -v '.ssh'); do
-  rm -f ~/$i
-  ln -s $PWD/$i ~/$i
+    rm -f ~/$i
+    ln -s $PWD/$i ~/$i
 done
 ln -s $PWD/.git-completion.bash ~
 ln -s $PWD/.gitconfig ~
@@ -13,8 +13,8 @@ mkdir -p ~/.config
 ln -s ~/HomeDir/.config/lazyvim ~/.config/nvim
 ln -s ~/HomeDir/.config/sway ~/.config/sway
 ln -s ~/HomeDir/.config/ghostty ~/.config/ghostty
+ln -s ~/HomeDir/.config/aerospace ~/.config/aerospace
 mkdir -p ~/.local/share
 ln -s ~/HomeDir/.local/share/i3status-rust ~/.local/share/
 curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux -o ~/.local/bin/rust-analyzer
 chmod +x ~/.local/bin/rust-analyzer
-ln -s ~/HomeDir/.alacritty.toml ~/.alacritty.toml
