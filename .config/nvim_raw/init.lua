b42ea9541556fab2bde1655cdfdd1f085d96b429
Dylan Reid <dgreid@dylanreid.com>
2025-02-19 21:07:51 -0800

    fzflua mappings

--- .config/nvim_raw/init.lua
+++ .config/nvim_raw/init.lua
@@ -1,6 +1,9 @@
 -- set color scheme to gruvbox
 vim.cmd("colorscheme gruvbox")
 
+-- set leader to space
+vim.g.mapleader = " "
+
 -- LSP setups
 local lspconfig = require("lspconfig")
 lspconfig.rust_analyzer.setup({
@@ -9,3 +12,7 @@ lspconfig.rust_analyzer.setup({
 		["rust-analyzer"] = {},
 	},
 })
+
+-- fzf keybindings
+vim.keymap.set("n", "<leader>fF", "<cmd>FzfLua files<CR>", { noremap = true, desc = "fzf files cwd" })
+vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files cwd=/<CR>", { noremap = true, desc = "fzf files root" })
